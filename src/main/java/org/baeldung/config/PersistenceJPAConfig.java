f6a2feaeb6fd29516b140b9f70cfa8bc8e2709cb
Brooke Smith <brooke@oehmsmith.com>
2015-03-30 10:30:31 +1100

    Works - mvn verify

---
+++ src/main/java/org/baeldung/config/PersistenceJPAConfig.java
@@ -0,0 +1,101 @@
+package org.baeldung.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.google.common.base.Preconditions;
+import com.jolbox.bonecp.BoneCPDataSource;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource({ "classpath:persistence-mysql.properties" })
+@ComponentScan({ "org.baeldung.persistence" })
+public class PersistenceJPAConfig {
+
+	private static final String PROPERTY_NAME_DATABASE_DRIVER = "jdbc.driverClassName";
+	private static final String PROPERTY_NAME_DATABASE_URL = "jdbc.url";
+	private static final String PROPERTY_NAME_DATABASE_USERNAME = "jdbc.user";
+	private static final String PROPERTY_NAME_DATABASE_PASSWORD = "jdbc.pass";
+	@Autowired
+	private Environment env;
+
+	public PersistenceJPAConfig() {
+		super();
+	}
+
+	// beans
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "org.baeldung.persistence.model" });
+
+		final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+
+		return em;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		final BoneCPDataSource dataSource = new BoneCPDataSource();
+		// final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClass(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+		dataSource.setJdbcUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+		dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+		dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+
+		return dataSource;
+	}
+
+	// @Bean
+	public DataSource dataSourceDM() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(Preconditions.checkNotNull(env.getProperty("jdbc.driverClassName")));
+		dataSource.setUrl(Preconditions.checkNotNull(env.getProperty("jdbc.url")));
+		dataSource.setUsername(Preconditions.checkNotNull(env.getProperty("jdbc.user")));
+		dataSource.setPassword(Preconditions.checkNotNull(env.getProperty("jdbc.pass")));
+
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
+		final JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	final Properties additionalProperties() {
+		final Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		// hibernateProperties.setProperty("hibernate.globally_quoted_identifiers", "true");
+		return hibernateProperties;
+	}
+
+}
